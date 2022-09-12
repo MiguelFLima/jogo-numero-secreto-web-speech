951d7ad172b8e50cdb6d7be0aaa24953cfc5ee4e
MiguelFLima <101779470+MiguelFLima@users.noreply.github.com>
2022-09-12 09:54:36 -0300

    Add files via upload

---
+++ app/validacao.js
@@ -0,0 +1,46 @@
+function verificaSeOChutePossuiUmValorValido() {
+    const numero = + chute
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+    }  
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `<div>Valor Inválido: O número precisa estar no range de ${menorValor} e ${maiorValor}</div>` 
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era <strong>${numero}</strong></h3>
+
+            <button id ='jogar-novamente' class='btn-jogar'>Jogar novamente!</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class='fa-solid fa-down-long'></div>
+        ` 
+    } else if (numero < numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class='fa-solid fa-up-long'></div>
+        ` 
+    }
+        
+    
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
